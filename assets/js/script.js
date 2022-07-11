2b8fc7ba0d9aa7bb5863ff5757b559e3509fe333
mayankkuthar <gabbar8055neel@gmail.com>
2022-07-11 17:12:45 +0530

    Updated Landing Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,3 @@
-// var swiper = new Swiper(".testimonial-slider", {
-//   grabCursor: true,
-//   centeredSlides: true,
-//   spaceBetween: 20,
-//   loop: true,
-//   autoplay: {
-//     delay: 9500,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     0: {
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerView: 1,
-//     },
-//     1024: {
-//       slidesPerView: 1,
-//     },
-//   },
-// });
 
 var swiper = new Swiper(".testimonial-slider", {
   grabCursor: true,
@@ -52,3 +27,75 @@ var swiper = new Swiper(".testimonial-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function nextDiv() {
+  var data_eng = document.getElementById("data_engg");
+  var visual_ele = document.getElementById("visual");
+  var market_ele = document.getElementById("market");
+  var cloud_ele = document.getElementById("cloud");
+  var data_eng_title = document.getElementById("data_engg_title");
+  var visual_ele_title = document.getElementById("visual_title");
+  var market_ele_title = document.getElementById("market_title");
+  var cloud_ele_title = document.getElementById("cloud_title");
+
+  if(data_eng.classList.contains("active")){
+    data_eng.classList.remove("active");
+    visual_ele.classList.add("active");
+    data_eng_title.classList.remove("active");
+    visual_ele_title.classList.add("active");
+  }
+  else if(visual_ele.classList.contains("active")){
+    visual_ele.classList.remove("active");
+    market_ele.classList.add("active");
+    visual_ele_title.classList.remove("active");
+    market_ele_title.classList.add("active");
+  }
+  else if(market_ele.classList.contains("active")){
+    market_ele.classList.remove("active");
+    cloud_ele.classList.add("active");
+    market_ele_title.classList.remove("active");
+    cloud_ele_title.classList.add("active");
+  }
+  else if(cloud_ele.classList.contains("active")){
+    cloud_ele.classList.remove("active");
+    data_eng.classList.add("active");
+    cloud_ele_title.classList.remove("active");
+    data_eng_title.classList.add("active");
+  }
+}
+
+function prevDiv() {
+  var data_eng = document.getElementById("data_engg");
+  var visual_ele = document.getElementById("visual");
+  var market_ele = document.getElementById("market");
+  var cloud_ele = document.getElementById("cloud");
+  var data_eng_title = document.getElementById("data_engg_title");
+  var visual_ele_title = document.getElementById("visual_title");
+  var market_ele_title = document.getElementById("market_title");
+  var cloud_ele_title = document.getElementById("cloud_title");
+
+  if(data_eng.classList.contains("active")){
+    data_eng.classList.remove("active");
+    cloud_ele.classList.add("active");
+    data_eng_title.classList.remove("active");
+    cloud_ele_title.classList.add("active");
+  }
+  else if(visual_ele.classList.contains("active")){
+    visual_ele.classList.remove("active");
+    data_eng.classList.add("active");
+    visual_ele_title.classList.remove("active");
+    data_eng_title.classList.add("active");
+  }
+  else if(market_ele.classList.contains("active")){
+    market_ele.classList.remove("active");
+    visual_ele.classList.add("active");
+    market_ele_title.classList.remove("active");
+    visual_ele_title.classList.add("active");
+  }
+  else if(cloud_ele.classList.contains("active")){
+    cloud_ele.classList.remove("active");
+    market_ele.classList.add("active");
+    cloud_ele_title.classList.remove("active");
+    market_ele_title.classList.add("active");
+  }
+}
